b717a9ddd82a2550e62b41d938aab76a46fff5c9
Jon Borgonia <jon@gomagames.com>
2017-01-10 20:11:02 -1000

    OP:REGISTER client management

---
+++ OP.js
@@ -0,0 +1,24 @@
+const ERROR = 'ERROR';
+const REGISTER = 'REGISTER';
+const REGISTERACK = 'REGISTERACK';
+
+const parse = message => {
+  let parsedMessage = JSON.parse(message);
+  if( !parsedMessage.hasOwnProperty('OP') ){
+    throw new Error('Improperly formatted OP message.');
+  }
+  return parsedMessage;
+};
+
+const create = (OP, payload) => JSON.stringify({
+  OP,
+  payload,
+});
+
+module.exports = {
+  create,
+  parse,
+  ERROR,
+  REGISTER,
+  REGISTERACK,
+};
--- index.js
+++ index.js
@@ -5,19 +5,53 @@ const { Server : WebSocketServer } = require('ws');
 const server = require('http').createServer();
 const wss = new WebSocketServer({ server });
 const PORT = process.env.PORT || 3000;
+const OP = require('./OP');
+
+// "username" => client
+const players = new Map();
 
 app.get('/api/hello', (req, res) => {
   const hello = 'world';
   res.json({ hello });
 });
 
+function clientReceiveMessage( message ){
+  let msg;
+  try{
+    msg = OP.parse(message);
+  }catch(error){
+    console.error(error);
+    return this.send(OP.create(OP.ERROR, { error }));
+  }
+
+  // trap unregistered users
+  if( this.username === null ){
+    // wait for OP:REGISTER
+    if( msg.OP === OP.REGISTER ){
+      // add the player to players
+      if( players.has(msg.payload.username) ){
+        // player name is taken
+        const error = `username: '${msg.payload.username}' is not available.`;
+        this.send(OP.create(OP.ERROR, { error }));
+      } else {
+        // username is available, register the player
+        this.username = msg.payload.username;
+        players.set(this.username, this);
+        this.send(OP.create(OP.REGISTERACK));
+      }
+    } else {
+      const error = `You are not registered yet. Register with OP:REGISTER first.`;
+      this.send(OP.create(OP.ERROR, { error }));
+    }
+    return; // trap
+  }
+}
+
 wss.on('connection', client => {
+  client.username = null;
 
-  client.on('message', message  => {
-    console.log('received: %s', message);
-  });
+  client.on('message', clientReceiveMessage.bind(client));
 
-  client.send('connected to ws server');
 });
 
 server.on('request', app);
