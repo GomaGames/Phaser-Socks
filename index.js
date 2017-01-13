1904213890ed4dee3cfc63d397fb75116d7f420a
Jon Borgonia <jon@gomagames.com>
2017-01-12 21:04:27 -1000

    add tons of error handling

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function clientReceiveMessage( message ){
     msg = OP.parse(message);
   }catch(error){
     console.error(error);
-    return this.send(OP.create(OP.ERROR, { error }));
+    return this.sendOp(OP.ERROR, { error });
   }
 
   // trap unregistered users
@@ -34,26 +34,47 @@ function clientReceiveMessage( message ){
       if( players.has(msg.payload.username) ){
         // player name is taken
         const error = `username: '${msg.payload.username}' is not available.`;
-        this.send(OP.create(OP.ERROR, { error }));
+        this.sendOp(OP.ERROR, { error });
       } else {
         // username is available, register the player
         this.username = msg.payload.username;
         players.set(this.username, this);
-        this.send(OP.create(OP.REGISTERACK));
+        this.sendOp(OP.REGISTERACK);
       }
     } else {
       const error = `You are not registered yet. Register with OP:REGISTER first.`;
-      this.send(OP.create(OP.ERROR, { error }));
+      this.sendOp(OP.ERROR, { error });
     }
     return; // trap
   }
 
 }
 
+function clientDisconnect(){
+  if( this.username !== null ){
+    if( players.has(this.username) ){
+      players.delete(this.username);
+    }
+  }
+  console.info(`Client username:'${this.username}' has disconnected.`);
+}
+
+// handles errors
+function sendOp(op, payload){
+  this.send(OP.create(op, payload), error => {
+    if( error !== undefined ){
+      console.error(`Error writing to client socket`, error);
+      clientDisconnect.call(this);
+    }
+  });
+}
+
 wss.on('connection', client => {
   client.username = null;
+  client.sendOp = sendOp;
 
   client.on('message', clientReceiveMessage.bind(client));
+  client.on('close', clientDisconnect.bind(client));
 
 });
 
