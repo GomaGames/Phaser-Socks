9a9b3e8ac5236e81c8cbd8818a3e25c010325cd8
Jon Borgonia <jon@gomagames.com>
2017-01-12 20:55:43 -1000

    handle client messages and OP:REGISTER

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const OP = require('./public/js/OP');
 
 const { Server : WebSocketServer } = require('ws');
 // const Server = require('ws').WebSocketServer;
@@ -8,18 +9,52 @@ const wss = new WebSocketServer({ server });
 
 const PORT = process.env.PORT || 3000;
 
+// "username" => client
+const players = new Map();
+
 app.get('/api/hello', (req, res) => {
   const hello = 'world';
   res.json({ hello });
 });
 
+function clientReceiveMessage( message ){
+  let msg;
+  try{
+    msg = OP.parse(message);
+  }catch(error){
+    console.error(error);
+    return this.send(OP.create(OP.ERROR, { error }));
+  }
+
+  // trap unregistered users
+  if( this.username === null ){
+    // wait for OP:REGISTER
+    if( msg.OP === OP.REGISTER ){
+      // add the player to players
+      if( players.has(msg.payload.username) ){
+        // player name is taken
+        const error = `username: '${msg.payload.username}' is not available.`;
+        this.send(OP.create(OP.ERROR, { error }));
+      } else {
+        // username is available, register the player
+        this.username = msg.payload.username;
+        players.set(this.username, this);
+        this.send(OP.create(OP.REGISTERACK));
+      }
+    } else {
+      const error = `You are not registered yet. Register with OP:REGISTER first.`;
+      this.send(OP.create(OP.ERROR, { error }));
+    }
+    return; // trap
+  }
+
+}
+
 wss.on('connection', client => {
+  client.username = null;
 
-  client.on('message', message  => {
-    console.log('received: %s', message);
-  });
+  client.on('message', clientReceiveMessage.bind(client));
 
-  client.send('connected to ws server');
 });
 
 server.on('request', app);
---
+++ public/js/OP.js
@@ -0,0 +1,50 @@
+(function(){
+
+  /*
+   * Helper methods
+   */
+  const parse = message => {
+    let parsedMessage = JSON.parse(message);
+    if( !parsedMessage.hasOwnProperty('OP') ){
+      throw new Error('Improperly formatted OP message.');
+    }
+    return parsedMessage;
+  };
+
+  const create = (OP, payload) => JSON.stringify({
+    OP,
+    payload,
+  });
+
+  /*
+   * OP codes
+   */
+  const ERROR = 'ERROR';
+  const REGISTER = 'REGISTER';
+  const REGISTERACK = 'REGISTERACK';
+
+  /*
+   * the module
+   */
+  const OP = {
+    create,
+    parse,
+    ERROR,
+    REGISTER,
+    REGISTERACK,
+  };
+
+
+  /* Make this module available to Node and Browser */
+  const root = this;
+  if( typeof exports !== 'undefined' ) {
+    if( typeof module !== 'undefined' && module.exports ) {
+      exports = module.exports = OP;
+    }
+    exports.OP = OP;
+  }
+  else {
+    root.OP = OP;
+  }
+
+}).call(this);
